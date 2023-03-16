1f39bfe145e05cee80bdc3ef1ce27d779a7f48f3
Amine <amine.tifour.1973@gmail.com>
2023-03-16 13:23:18 +0100

    final_commit

---
+++ README.md
@@ -0,0 +1,35 @@
+# 2FA UI
+TP pour mes étudiant(s). Exercice d'apprentissage de JavaScript
+
+---
+
+![code](https://sosdevtips.b-cdn.net/github-badges/JUNIOR.svg) ![level](https://sosdevtips.b-cdn.net/github-badges/JAVASCRIPT.svg)
+
+Les instructions pour la réalisation de ce TP sont simples :
+
+Clonez ce dépôt dans votre répertoire de travail et ouvrez-le dans votre éditeur de code préféré.
+L'interface est déjà prête, cependant, il faut encore ajouter les fonctionnalités. Pour cela, vous devez modifier le fichier main.js
+
+## Les résultats attendus sont les suivants :
+
+À l'aide des connaissances que vous avez acquises jusqu'à présent, l'utilisateur doit pouvoir :
+
+- saisir uniquement 1 chiffre par input
+- valider la saisie en appuyant sur le bouton "Confirmer le code"
+- voir une erreur si la saisie est incorrecte
+
+De votre côté, vous devez :
+
+- vérifier que la saisie est correcte
+- stocker le code dans une variable
+- afficher le code saisie dans la console
+- afficher un message de succès si la saisie est correcte
+
+---
+
+## Comment rendre ta réalisation ?
+1. Publie ton code dans un dépôt GitHub public et envoie-moi le lien du TP.
+2. Déploie également le projet sur GitHub Pages
+
+### Conseils
+Rédige ton algorithme, fais des recherches sur le web, parcours mes dépôts GitHub et surtout lis la documentation MDN.
---
+++ mains.js
@@ -0,0 +1,49 @@
+let case1 = document.querySelector('#number-1')
+let case2 = document.querySelector('#number-2')
+let case3 = document.querySelector('#number-3')
+let case4 = document.querySelector('#number-4')
+let case5 = document.querySelector('#number-5')
+let case6 = document.querySelector('#number-6')
+
+
+// let test = " 7"
+
+// if(!test){
+//     console.log("il est vide")
+// }else if(test){
+//     console.log("il est plein")
+// }
+
+// let testo = "1"
+// if(testo.match(regex)){
+    //     console.log("c'est bon")
+    // }
+    
+    // let testa = 1
+    // console.log(testa)
+    
+
+
+let regex = /^[1-9]{1}/
+
+function SendCode(){
+    if(!case1.value.trim() && !case2.value.trim() && !case3.value.trim() && !case4.value.trim() && !case5.value.trim() && !case6.value.trim()){
+        alert('veuillez entrer le code')
+    }else{
+        if(case1.value.match(regex) && case2.value.match(regex) && case3.value.match(regex) && case4.value.match(regex) && case5.value.match(regex) && case6.value.match(regex)){
+            alert("Merci de votre coopération")
+        }else{
+            alert('veuillez entrer des chiffres svp')
+        }
+    }
+}
+
+// document.querySelector('.foward').addEventListener('click', SendCode())
+// function SendCode(){
+//     console.log(case1.value)
+//     console.log(case3.value)
+//     console.log(case4.value)
+//     console.log(case2.value)
+//     console.log(case5.value)
+//     console.log(case6.value)
+// }
